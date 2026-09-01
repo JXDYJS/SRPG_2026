ae8a8e91a013bbc5ef2081684bbe6c9e546e73f6
YJS <halashu1@outlook.com>
2026-09-01 21:55:06 +0800

    fix(irc): bind _IRCCachePrev when dispatching IRCEProbe

--- Assets/Script/Editor/IrradianceCacheInputLog.cs
+++ Assets/Script/Editor/IrradianceCacheInputLog.cs
@@ -214,6 +214,7 @@ namespace Render.EditorTools
             var cb = new CommandBuffer();
             try
             {
+                cb.SetComputeTextureParam(feature.BakeCS, s_eProbeKernel, "_IRCCachePrev", feature.ReadCache);
                 cb.SetComputeBufferParam(feature.BakeCS, s_eProbeKernel, "_IRCEProbeOut", s_eProbeBuf);
                 cb.SetComputeVectorParam(feature.BakeCS, Shader.PropertyToID("_IRCEProbeTexels"), s_eProbeTexels);
                 cb.DispatchCompute(feature.BakeCS, s_eProbeKernel, 1, 1, 1);
